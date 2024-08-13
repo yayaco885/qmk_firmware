9ebbbaa7c33d9a12e7745c082104bb2dfa49cb0b
ya <ana_jp1@hotmail.com>
2024-08-13 13:34:12 +0900

    fix: config.h - add TAPPING_TERM 50

--- keyboards/crkbd/keymaps/default/config.h
+++ keyboards/crkbd/keymaps/default/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 //#define QUICK_TAP_TERM 0
-//#define TAPPING_TERM 100
+#define TAPPING_TERM 50
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING

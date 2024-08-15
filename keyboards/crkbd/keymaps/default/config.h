186e08f1d47c5219bac5dcdbc6f4f9b6b0404a00
ya <ana_jp1@hotmail.com>
2024-08-15 17:29:09 +0900

    fix: config.h - add #define USB_POLLING_INTERVAL_MS 1

--- keyboards/crkbd/keymaps/default/config.h
+++ keyboards/crkbd/keymaps/default/config.h
@@ -31,6 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define QUICK_TAP_TERM 0
 #define TAPPING_TERM 150
 #define PERMISSIVE_HOLD
+#define USB_POLLING_INTERVAL_MS 1
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
